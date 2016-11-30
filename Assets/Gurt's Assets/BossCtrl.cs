dbcf909a34845c8f4454de0e9cb1c0e130faa94f
GurtBass <revgsb@gmail.com>
2016-11-30 15:46:02 -0500

    Boss controls and assets

---
+++ Assets/Gurt's Assets/BossCtrl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossCtrl : MonoBehaviour {
+
+    public Vector3 inputs;  // axis values will be stored here so we can see them change in the inspector
+    public bool actionButton;
+
+    private Animator anim;  // our animator component, with the animCtrl for the hero... but how to set this reference?
+
+    public void OnEnable()
+    {
+        anim = this.gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void BossCtrlUpdate()
+    {
+
+        actionButton = Input.GetButtonDown("Attack");
+
+        // animate the skinned mesh for the knight/hero based on user input
+        if (anim != null)
+        {
+            if (actionButton == true)
+                anim.SetTrigger("action");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        BossCtrlUpdate();
+    }
+}
